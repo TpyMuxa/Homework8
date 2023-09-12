67c56070f164bc06edba17d4f60a54f782e5c09b
TpyMuxa <tpymuxa1133@gmail.com>
2023-09-12 22:16:57 +0500

    Задание 1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
 public class Main {
     public static void main(String[] args) {
 
+        System.out.println("Задание 1");
+
+        int [] boxInt = new int[3];
+        boxInt [0] = 1;
+        boxInt [1] = 2;
+        boxInt [2] = 3;
+
+        double [] boxDouble = {1.57, 7.654, 9.986};
+
+        char [] boxChar = {'h', 'e', 'l', 'l', 'o'};
     }
 }
